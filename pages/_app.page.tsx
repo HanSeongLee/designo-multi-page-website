2472cf70843ae75e77ca051e470fcf65aa9a83db
HanSeongLee <jkhanseong@gmail.com>
2023-06-23 21:28:04 +0900

    ADD | Font

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,8 +1,23 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { Jost } from 'next/font/google';
+
+const jost = Jost({
+  weight: ['400', '500'],
+  subsets: ['latin'],
+});
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+      <>
+        <style jsx global>{`
+          :root {
+            --jost-font: ${jost.style.fontFamily};
+          }
+        `}</style>
+        <Component {...pageProps} />
+      </>
+  );
 }
 
 export default MyApp
