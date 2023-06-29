5568480a3243cdc9031e8184bbf3e7d27b1f69b7
HanSeongLee <jkhanseong@gmail.com>
2023-06-29 21:22:58 +0900

    ADD | Button component

---
+++ components/commons/Button/index.tsx
@@ -0,0 +1,37 @@
+import React, { ElementType } from 'react';
+import styles from './style.module.scss';
+import cn from 'classnames';
+import HamburgerIcon from 'public/icons/icon-hamburger.svg'
+
+export enum ButtonVariant {
+    Icon = 'icon',
+}
+
+export enum ButtonIcon {
+    Hamburger = 'hamburger',
+}
+
+interface IProps<T extends ElementType = 'button' | 'a'> {
+    as?: T;
+    variant: ButtonVariant;
+    icon: ButtonIcon;
+    className?: string;
+    href?: string;
+    onClick?: () => void;
+}
+
+const Button: React.FC<IProps> = ({
+                                      as = 'button', variant, icon, className,
+                                      children, ...props
+                                  }) => {
+    return React.createElement(as, {
+        className: cn(styles.button, {
+            [styles.icon]: variant === ButtonVariant.Icon,
+        }, className),
+        ...props,
+    }, variant === ButtonVariant.Icon ? (<>
+        {icon === ButtonIcon.Hamburger && <HamburgerIcon />}
+    </>) : children);
+};
+
+export default Button;
