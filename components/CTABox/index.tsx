2f1dc3b23074db4bb86047b54718534456915a43
HanSeongLee <jkhanseong@gmail.com>
2023-07-09 14:28:53 +0900

    ADD | CTABox component

---
+++ components/CTABox/index.tsx
@@ -0,0 +1,44 @@
+import React, { HTMLAttributes } from 'react';
+import styles from './style.module.scss';
+import cn from 'classnames';
+import Heading, { HeadingAs, HeadingType } from 'components/commons/Heading';
+import Paragraph, { ParagraphType } from 'components/commons/Paragraph';
+import Button, { ButtonVariant } from 'components/commons/Button';
+
+interface IProp extends HTMLAttributes<HTMLDivElement> {
+    title: string;
+    description: string;
+    buttonName: string;
+    link: string;
+}
+
+const CTABox: React.FC<IProp> = ({
+                                     title, description, buttonName, link,
+                                     className, ...props
+                                 }) => {
+    return (
+        <div className={cn(styles.ctaBox, className)}
+             {...props}
+        >
+            <Heading className={styles.title}
+                     as={HeadingAs.H2}
+                     type={HeadingType.H1}
+            >
+                {title}
+            </Heading>
+            <Paragraph className={styles.description}
+                       type={ParagraphType.P2}
+            >
+                {description}
+            </Paragraph>
+            <Button as={'a'}
+                    href={link}
+                    variant={ButtonVariant.Secondary}
+            >
+                {buttonName}
+            </Button>
+        </div>
+    );
+};
+
+export default CTABox;
