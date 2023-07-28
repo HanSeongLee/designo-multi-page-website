562fea15817970f3e3c82bbfe8dbbd269642ce98
HanSeongLee <jkhanseong@gmail.com>
2023-07-28 22:09:43 +0900

    ADD | Contact page

---
+++ pages/contact/index.page.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './style.module.scss';
+import { NextPage } from 'next';
+import Header from 'components/commons/Header';
+import Footer from 'components/commons/Footer';
+import LocationSection from 'components/LocationSection';
+import IntroSection from 'components/IntroSection';
+import ContactFormContainer from 'containers/forms/ContactFormContainer';
+
+const ContactPage: NextPage = () => {
+    return (
+        <>
+            <Header />
+            <main className={styles.main}>
+                <section>
+                    <IntroSection title={'Contact Us'}
+                                  description={'Ready to take it to the next level? Let’s talk about your project or idea and find out how we can help your business grow. If you are looking for unique digital experiences that’s relatable to your users, drop us a line.'}
+                    >
+                        <ContactFormContainer className={styles.contactFormContainer} />
+                    </IntroSection>
+                </section>
+                <LocationSection />
+            </main>
+            <Footer />
+        </>
+    );
+};
+
+export default ContactPage;
